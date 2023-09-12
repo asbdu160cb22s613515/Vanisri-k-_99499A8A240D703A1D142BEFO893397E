aca384148706b9044c32c7b2c72e43a3237ffd69
asbdu160cb22s613515 <144446593+asbdu160cb22s613515@users.noreply.github.com>
2023-09-12 12:09:30 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,17 @@
+
+
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
+
+# Example usage:
+products = ["Laptop", "Tablet", "Phone", "Phone", "Desktop"]
+target_product = "Phone"
+result = linear_search_product(products, target_product)
+print(result)
+
+
+
